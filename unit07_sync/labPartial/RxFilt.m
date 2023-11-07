8e3c502d2c16f00349b62dce7348e593407ae5de
sdrangan <sdrangan@gmail.com>
2023-11-07 10:06:13 -0500

    Updated the labs for the new TxFilt and RxFilt classes.

--- unit07_sync/labPartial/RxFilt.m
+++ unit07_sync/labPartial/RxFilt.m
@@ -8,30 +8,39 @@ classdef RxFilt < matlab.System
         rateOut;    % Output rate in Hz
 
         % Filter parameters
-        sigBW;        % Signal BW in Hz
-        Astop = 40;   % Stopband attenuation in dB
-        bfilt;        % Filter coefficients
- 
+        Astop;        % Stopband attenuation in dB
+        Fp, Fst;      % Passband and stopband frequency in Hz
+        bfilt;        % Filter coefficients        
+
+        % ADC parameters
+        rxScale;      % Flag indicating if data is to be scaled
+        rxFS;         % Rx output full-scale level
+        
     end
 
     methods
         function obj = RxFilt(opt)
             % Constructor
-
             arguments
                 opt.ovRatio (1,1) {mustBeInteger} = 2;
-                opt.rateIn (1,1) {mustBeNumeric} = 40e6;
-                opt.sigBW = [];
+                opt.rateIn (1,1) {mustBeNumeric} = 20e6;
+                opt.Fp (1,1) {mustBeNumeric} = 20e6;
+                opt.Fst (1,1) {mustBeNumeric} = 25e6;               
+                opt.Astop (1,1) {mustBeNumeric} = 40;
+                opt.rxFS (1,1) {mustBeNumeric} = 2^12;
+                opt.rxScale (1,1) {boolean} = false;                
             end
 
             % TxFilt Constructor
             obj.ovRatio = opt.ovRatio;
-            obj.rateIn = opt.rateIn;
+            obj.rateIn = opt.rateIn;        
+            obj.Fp = opt.Fp;
+            obj.Fst = opt.Fst;
             obj.rateOut = obj.rateIn / obj.ovRatio;
-            if (isempty(opt.sigBW))
-                opt.sigBW = 0.95*opt.rateOut;
-            end
-            obj.sigBW = opt.sigBW;            
+            obj.Astop = opt.Astop;         
+            obj.rxScale = opt.rxScale;
+            obj.rxFS = opt.rxFS;
+            
 
         end
 
@@ -40,24 +49,44 @@ classdef RxFilt < matlab.System
     methods (Access = protected)
         function y = stepImpl(obj, x)
 
-            % Design the downsampling filter if it is has not yet
+            % Design the upsampling filter if it is has not yet
             % been designed
-            if isempty(obj.bfilt)
-                Ap = obj.ovRatio;
-                Hd = fdesign.lowpass('Fp,Fst,Ap,Ast',...
-                    obj.sigBW/2,obj.rateOut/2,Ap,obj.Astop,obj.rateIn);
-                d = design(Hd,'equiripple');
-                obj.bfilt = d.numerator;
+            if isempty(obj.bfilt) && (obj.ovRatio > 1)
+                obj.designFilter();
+                
             end
 
+            % Scale the samples.  This is used for the SDR
+            if obj.rxScale
+                x = double(x) / obj.rxFS;
+            end
+
+
             % Filter and downsample
-            y = conv(obj.bfilt, x);
-            y = downsample(y(2:end),obj.ovRatio);
+            if obj.ovRatio > 1
+                y = conv(obj.bfilt, x);
+                y = downsample(y(2:end),obj.ovRatio);
+            else
+                y = x;
+            end
        
         end
 
 
     end
+
+    methods
+        function designFilter(obj)           
+            % Design the filter and store the filter coefficients
+            Ap = 1;  % Passband ripple
+            Hd = fdesign.lowpass('Fp,Fst,Ap,Ast',...
+                obj.Fp, obj.Fst, Ap, obj.Astop, obj.rateIn);
+            d = design(Hd,'equiripple');
+            obj.bfilt = d.numerator;
+        end
+
+    end
+
 
 
 end
--- unit07_sync/labPartial/TxFilt.m
+++ unit07_sync/labPartial/TxFilt.m
@@ -8,13 +8,13 @@ classdef TxFilt < matlab.System
         rateOut;    % Output rate in Hz
 
         % Filter parameters
-        sigBW;        % Signal BW in Hz
-        Astop = 40;   % Stopband attenuation in dB
+        Astop;        % Stopband attenuation in dB
+        Fp, Fst;      % Passband and stopband frequency in Hz
         bfilt;        % Filter coefficients
 
         % Backoff paramters
-        applyBackoff;  % Indicate if backoff is applied
-        dacFS;         % DAC full-scale level
+        backoffAutoScale;  % Indicate if backoff is auto-scaling is enabled
+        txFS;          % Tx input full-scale level
         backoffLev;    % Backoff level in dB
 
     end
@@ -26,23 +26,24 @@ classdef TxFilt < matlab.System
             arguments
                 opt.ovRatio (1,1) {mustBeInteger} = 2;
                 opt.rateIn (1,1) {mustBeNumeric} = 20e6;
-                opt.sigBW = [];
-                opt.applyBackoff (1,1) {boolean} = false;
-                opt.dacFS (1,1) {mustBeNumeric} = 1.0;
+                opt.Fp (1,1) {mustBeNumeric} = 20e6;
+                opt.Fst (1,1) {mustBeNumeric} = 25e6;
+                opt.backoffAutoScale (1,1) {boolean} = false;
+                opt.txFS (1,1) {mustBeNumeric} = 1.0;
                 opt.backoffLev (1,1) {mustBeNumeric} = 9.0;
+                opt.Astop (1,1) {mustBeNumeric} = 40;
             end
 
             % TxFilt Constructor
             obj.ovRatio = opt.ovRatio;
-            obj.rateIn = opt.rateIn;
-            if (isempty(opt.sigBW))
-                opt.sigBW = 0.9*opt.rateIn;
-            end
-            obj.sigBW = opt.sigBW;
+            obj.rateIn = opt.rateIn;        
+            obj.Fp = opt.Fp;
+            obj.Fst = opt.Fst;
             obj.rateOut = obj.ovRatio * obj.rateIn;
-            obj.applyBackoff = opt.applyBackoff;
-            obj.dacFS = opt.dacFS;
+            obj.backoffAutoScale = opt.backoffAutoScale;
+            obj.txFS = opt.txFS;
             obj.backoffLev = opt.backoffLev;
+            obj.Astop = opt.Astop;
 
         end
 
@@ -53,43 +54,56 @@ classdef TxFilt < matlab.System
 
             % Design the upsampling filter if it is has not yet
             % been designed
-            if isempty(obj.bfilt)
+            if isempty(obj.bfilt) && (obj.ovRatio > 1)
+                obj.designFilter();
                 
-                % TODO:  Set the filter parameters
-                %
-                %    Ap = 1;  % Passband gain (linear scale)
-                %    Ast = obj.Astop;  % Stopband rejection in dB
-                %    Fst = ... % stopband bandwidth in Hz
-                %    Fp = ...  % passband bandwidth in Hz                
-
-                % Design the filter and get the filter coefficients
-                Hd = fdesign.lowpass('Fp,Fst,Ap,Ast',...
-                    Fp, Fst, Ap, Ast, obj.rateOut);
-                d = design(Hd,'equiripple');
-                obj.bfilt = d.numerator;
             end
 
-            % TODO:  Upsample by obj.ovRation and filter with obj.bfilt
-            %    y = upsample(x, ...);
-            %    y = conv(...);
+            if (obj.ovRatio > 1)
+                % TODO:  Upsample by obj.ovRatio and filter with obj.bfilt
+                %    x1 = upsample(x, ...);
+                %    y0 = conv(...);
+                x1 = upsample(x, obj.ovRatio);
+                y0 = conv(obj.bfilt, x1);
+            else
+                y0 = x;
+            end
 
             % Apply backoff
-            if obj.applyBackoff
+            if obj.backoffAutoScale
                 % TODO:  Rescale the output to the desired backoff
                 % Select a scale factor scale such that after:
                 %
-                %     y = scale*y;
+                %     y1 = scale*y0;
                 %
-                % you should get that 
+                % you should get that
                 %
-                %    mean(abs(y).^2) = 2*obj.dacFs^2*db2pow(-obj.backoffLev)
+                %    mean(abs(y1).^2) = 2*obj.txFs^2*db2pow(-obj.backoffLev)
+                scale = obj.txFS*sqrt(2*db2pow(-obj.backoffLev)/mean(abs(y0).^2));
+                y1 = scale*y0;
 
                 % TODO:  Clip the output so that real(y) and imag(y)
-                %   are in the range [-obj.dacFS, obj.dacFS]    
+                %   are in the range [-obj.txFS, obj.txFS]
                 %    y = ...
+                y = min(max(real(y1), -obj.txFS), obj.txFS) + ...
+                    1i*min(max(imag(y1),-obj.txFS), obj.txFS);
+            else
+                y = y0;
             end
         end
 
+
+    end
+
+    methods
+        function designFilter(obj)           
+            % Design the filter and store the filter coefficients
+            Ap = 1;  % Passband ripple
+            Hd = fdesign.lowpass('Fp,Fst,Ap,Ast',...
+                obj.Fp, obj.Fst, Ap, obj.Astop, obj.rateOut);
+            d = design(Hd,'equiripple');
+            obj.bfilt = d.numerator;
+        end
 
     end
 
